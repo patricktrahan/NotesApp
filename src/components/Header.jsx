52904de66096cd8fb5b37f0a7b1ac24fe6550b2a
Patrick Trahan <34581132+patricktrahan@users.noreply.github.com>
2025-09-09 13:12:03 -0500

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NoteIcon from "@mui/icons-material/Note";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        {" "}
+        <NoteIcon />
+        Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
